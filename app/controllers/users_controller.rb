b90499548da54159953241e6f92416c35eb740a6
Your Name <your.email@example.com>
2019-02-21 05:18:13 +0000

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
-        flash[:success] = "Welcome to the Sample App!"
-       redirect_to @user
-      # 保存の成功をここで扱う。
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
-  
-  
-   private
+
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
