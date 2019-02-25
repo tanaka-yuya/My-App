712f3dbf3f95d2fc63de15685dedfe58b5a6672b
Your Name <your.email@example.com>
2019-02-25 08:50:13 +0000

    #1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(name:  "Example User",
+User.create!(name:  "Example User Test",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
