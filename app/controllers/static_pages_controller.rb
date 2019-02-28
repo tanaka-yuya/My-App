520fdff4c8e24ee6da9ee0d47ff74a194d546c6f
Your Name <your.email@example.com>
2019-02-28 05:13:57 +0000

    resolve not sending email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
 
-   def home
+  def home
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
